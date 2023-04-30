fa3e43462a64a87cdb71a3bb0719d0c9ee5e1c3b
Dor Shir <117517821+Dorshir@users.noreply.github.com>
2023-04-30 09:04:52 +0000

    commit11

--- stshell.c
+++ stshell.c
@@ -1,20 +1,4 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <signal.h>
-
-#define MAX_LINE 1024 // The maximum length command
-#define clear() printf("\033[H\033[J") // Clearing the shell using escape sequences
-
-int should_run = 1;  // flag to determine when to exit program
-int should_wait = 1; // flag to determine if process should run in the background
+#include "stshell.h"
 
 /**
  * Opening message during startup shell
