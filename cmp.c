3b5f510ce7840060a9ae50ecea7b2020e3d63a50
Dor Shir <117517821+Dorshir@users.noreply.github.com>
2023-04-30 08:54:45 +0000

    commit10

--- cmp.c
+++ cmp.c
@@ -5,10 +5,12 @@
 
 #define MAX_SIZE 1024
 
+// Print the usage information for the program
 void print_usage() {
     printf("Usage: cmp <file1> <file2> [-i] (to ignore uppercase) [-v] (to output equalization)\n");
 }
 
+// Compare the contents of two files and return whether they are the same or different
 int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
     FILE *fp1, *fp2;
     int ch1, ch2;
@@ -27,28 +29,34 @@ int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
 
     int result = 0;
 
+    // Read each character from both files and compare them
     while (1) {
         ch1 = fgetc(fp1);
         ch2 = fgetc(fp2);
 
+        // If ignore_case is enabled, convert the characters to lowercase
         if (ignore_case) {
             ch1 = tolower(ch1);
             ch2 = tolower(ch2);
         }
 
-        if (ch1 == EOF && ch2 == EOF) {
-            break;
-        }
-
+        // If the characters are different, set the result to 1 and break out of the loop
         if (ch1 != ch2) {
             result = 1;
             break;
         }
+
+        // If we have reached the end of both files, break out of the loop
+        if (ch1 == EOF && ch2 == EOF) {
+            break;
+        }
     }
 
+    // Close the files
     fclose(fp1);
     fclose(fp2);
 
+    // If verbose is enabled, print the result of the comparison
     if (verbose) {
         if (result == 0) {
             printf("equal\n");
@@ -57,15 +65,19 @@ int compare_files(char* file1, char* file2, int ignore_case, int verbose) {
         }
     }
 
+    // Return the result of the comparison
     return result;
 }
 
+// The entry point of the program
 int main(int argc, char* argv[]) {
+    // Check if the number of arguments is correct
     if (argc < 3) {
         print_usage();
         exit(1);
     }
 
+    // Parse the arguments
     char* file1 = argv[1];
     char* file2 = argv[2];
     int ignore_case = 0;
@@ -83,6 +95,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Compare the files and return the result
     int result = compare_files(file1, file2, ignore_case, verbose);
     return result;
 }
--- copy.c
+++ copy.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Print the usage information for the program
 void print_usage() {
     printf("Usage: copy <file1> <file2> [-f] [-v]\n");
 }
 
+// Copy the contents of a source file to a destination file
 int copy_file(char* src, char* dst, int force, int verbose) {
     FILE *fp1, *fp2;
     char buffer[1024];
     size_t n;
 
+    // Open the source file for reading
     fp1 = fopen(src, "r");
     if (fp1 == NULL) {
         printf("Cannot open file %s\n", src);
         exit(1);
     }
 
+    // If the destination file already exists and force is not enabled, return an error
     fp2 = fopen(dst, "r");
     if (fp2 != NULL && !force) {
         printf("File %s already exists. Use -f to force overwrite.\n", dst);
@@ -25,6 +29,7 @@ int copy_file(char* src, char* dst, int force, int verbose) {
         return 1;
     }
 
+    // Open the destination file for writing
     fp2 = fopen(dst, "w");
     if (fp2 == NULL) {
         printf("Cannot create file %s\n", dst);
@@ -32,6 +37,7 @@ int copy_file(char* src, char* dst, int force, int verbose) {
         exit(1);
     }
 
+    // Read from the source file and write to the destination file
     while ((n = fread(buffer, 1, sizeof(buffer), fp1)) > 0) {
         if (fwrite(buffer, 1, n, fp2) != n) {
             printf("Error writing to file %s\n", dst);
@@ -41,22 +47,28 @@ int copy_file(char* src, char* dst, int force, int verbose) {
         }
     }
 
+    // Close the files
     fclose(fp1);
     fclose(fp2);
 
+    // If verbose is enabled, print a success message
     if (verbose) {
         printf("success\n");
     }
 
+    // Return 0 to indicate success
     return 0;
 }
 
+// The entry point of the program
 int main(int argc, char* argv[]) {
+    // Check if the number of arguments is correct
     if (argc < 3) {
         print_usage();
         exit(1);
     }
 
+    // Parse the arguments
     char* src = argv[1];
     char* dst = argv[2];
     int force = 0;
@@ -74,6 +86,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Copy the source file to the destination file and return the result
     int result = copy_file(src, dst, force, verbose);
     return result;
 }
